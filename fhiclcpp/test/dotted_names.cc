da7893a7592e0a818569e38ac1ab15a28577d3bd
Kyle Knoepfel <knoepfel@fnal.gov>
2021-02-10 09:21:45 -0600

    Resolve bug #25492: Improve checking dotted-name has_key support.

--- fhiclcpp/test/dotted_names.cc
+++ fhiclcpp/test/dotted_names.cc
@@ -1,17 +1,25 @@
+#define BOOST_TEST_MODULE (has_key of dotted names test)
+#include "cetlib/quiet_unit_test.hpp"
+
 #include "fhiclcpp/ParameterSet.h"
 #include "fhiclcpp/make_ParameterSet.h"
-#include <cassert>
+
 #include <string>
 
-int
-main()
+BOOST_AUTO_TEST_SUITE(dotted_names_test)
+
+BOOST_AUTO_TEST_CASE(dotted_names_t)
 {
   fhicl::ParameterSet ps;
   std::string text("m1: { val: 3 name: boo} ");
   fhicl::make_ParameterSet(text, ps);
 
-  assert(ps.has_key("m1.val"));
-  assert(!ps.has_key("m1.val.three"));
-  assert(ps.get<int>("m1.val") == 3);
-  assert(ps.get<std::string>("m1.name") == "boo");
+  BOOST_TEST(ps.has_key("m1.val"));
+  BOOST_TEST(!ps.has_key("m1.val.three"));
+  BOOST_TEST(ps.get<int>("m1.val") == 3);
+  BOOST_TEST(ps.get<std::string>("m1.name") == "boo");
+  BOOST_TEST(!ps.has_key("m0"));
+  BOOST_TEST(!ps.has_key("m0.nothing.to.see.here"));
 }
+
+BOOST_AUTO_TEST_SUITE_END()
