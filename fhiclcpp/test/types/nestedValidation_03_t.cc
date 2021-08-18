eed82e22f9bfa235ba715a2326d7473429f08538
Kyle Knoepfel <knoepfel@fnal.gov>
2021-08-18 16:43:53 -0500

    Boost test adjustments.

--- fhiclcpp/test/types/nestedValidation_03_t.cc
+++ fhiclcpp/test/types/nestedValidation_03_t.cc
@@ -42,7 +42,6 @@ namespace {
   validateConfig(std::string const& cfg)
   {
     auto const ps = ParameterSet::make(cfg);
-    ;
     Table<Config> validatedConfig{Name("validatedConfig")};
     validatedConfig.validate_ParameterSet(ps);
     return validatedConfig;
@@ -60,14 +59,14 @@ BOOST_AUTO_TEST_CASE(GoodTuple1)
   auto validatedConfig = validateConfig(good);
   auto const& pars = validatedConfig().nested().pars();
   BOOST_TEST_REQUIRE(pars.size() == 2ull);
-  BOOST_TEST_REQUIRE(std::get<0>(pars[0]) == "A"s);
-  BOOST_TEST_REQUIRE(std::get<1>(pars[0]) == ref1);
-  BOOST_TEST_REQUIRE(std::get<0>(pars[1]) == "X"s);
-  BOOST_TEST_REQUIRE(std::get<1>(pars[1]) == ref2);
+  BOOST_TEST(std::get<0>(pars[0]) == "A"s);
+  BOOST_TEST(std::get<1>(pars[0]) == ref1);
+  BOOST_TEST(std::get<0>(pars[1]) == "X"s);
+  BOOST_TEST(std::get<1>(pars[1]) == ref2);
   auto const& defaultedPars = validatedConfig().nestedWithDef().pars();
   BOOST_TEST_REQUIRE(defaultedPars.size() == 1ull);
-  BOOST_TEST_REQUIRE(std::get<0>(defaultedPars[0]) == "T"s);
-  BOOST_TEST_REQUIRE(std::get<1>(defaultedPars[0]) == ref3);
+  BOOST_TEST(std::get<0>(defaultedPars[0]) == "T"s);
+  BOOST_TEST(std::get<1>(defaultedPars[0]) == ref3);
 }
 
 BOOST_AUTO_TEST_CASE(GoodTuple2)
@@ -81,34 +80,37 @@ BOOST_AUTO_TEST_CASE(GoodTuple2)
   auto validatedConfig = validateConfig(good);
   auto const& pars = validatedConfig().nested().pars();
   BOOST_TEST_REQUIRE(pars.size() == 2ull);
-  BOOST_TEST_REQUIRE(std::get<0>(pars[0]) == "A"s);
-  BOOST_TEST_REQUIRE(std::get<1>(pars[0]) == ref1);
-  BOOST_TEST_REQUIRE(std::get<0>(pars[1]) == "X"s);
-  BOOST_TEST_REQUIRE(std::get<1>(pars[1]) == ref2);
+  BOOST_TEST(std::get<0>(pars[0]) == "A"s);
+  BOOST_TEST(std::get<1>(pars[0]) == ref1);
+  BOOST_TEST(std::get<0>(pars[1]) == "X"s);
+  BOOST_TEST(std::get<1>(pars[1]) == ref2);
   auto const& defaultedPars = validatedConfig().nestedWithDef().pars();
   BOOST_TEST_REQUIRE(defaultedPars.size() == 2ull);
-  BOOST_TEST_REQUIRE(std::get<0>(defaultedPars[0]) == "M"s);
-  BOOST_TEST_REQUIRE(std::get<1>(defaultedPars[0]) == ref3);
-  BOOST_TEST_REQUIRE(std::get<0>(defaultedPars[1]) == "R"s);
-  BOOST_TEST_REQUIRE(std::get<1>(defaultedPars[1]) == ref4);
+  BOOST_TEST(std::get<0>(defaultedPars[0]) == "M"s);
+  BOOST_TEST(std::get<1>(defaultedPars[0]) == ref3);
+  BOOST_TEST(std::get<0>(defaultedPars[1]) == "R"s);
+  BOOST_TEST(std::get<1>(defaultedPars[1]) == ref4);
 }
 
 BOOST_AUTO_TEST_CASE(BadTuple1)
 {
   string const bad{"nested: { pars: [[A], [X, [Y, Z]]] }"s};
   BOOST_REQUIRE_THROW(validateConfig(bad), detail::validationException);
+  BOOST_TEST_REQUIRE(NameStackRegistry::empty());
 }
 
 BOOST_AUTO_TEST_CASE(BadTuple2)
 {
   string const bad{"nested: { pars: [[A]] }"s};
   BOOST_REQUIRE_THROW(validateConfig(bad), detail::validationException);
+  BOOST_TEST_REQUIRE(NameStackRegistry::empty());
 }
 
 BOOST_AUTO_TEST_CASE(BadTuple3)
 {
   string const bad{"nested: { pars: [[A, 7]] }"s};
   BOOST_REQUIRE_THROW(validateConfig(bad), fhicl::exception);
+  BOOST_TEST_REQUIRE(NameStackRegistry::empty());
 }
 
 BOOST_AUTO_TEST_CASE(BadTuple4)
@@ -116,6 +118,7 @@ BOOST_AUTO_TEST_CASE(BadTuple4)
   string const bad{"nested: { pars: [[A, [7]]] }\n"s +
                    "nestedWithDef: { pars: [[A]]}"s};
   BOOST_REQUIRE_THROW(validateConfig(bad), detail::validationException);
+  BOOST_TEST_REQUIRE(NameStackRegistry::empty());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
